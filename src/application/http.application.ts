cf944c46b64659a7dab7c114c2004db76fcc8c0c
Kevin de Oliveira <contact@kevindeoliveira.com>
2021-09-04 15:19:08 -0400

    httpapp fix init

--- src/application/http.application.ts
+++ src/application/http.application.ts
@@ -63,8 +63,8 @@ export default class HttpApplication{
         if(middlewares && !Array.isArray(middlewares))
             throw new InvalidArgumentException("Middlewares must be provided as an array");
 
-        this.initializeControllers(controllers);
         this.initializeMiddlwares(middlewares);    
+        this.initializeControllers(controllers);
         
         this.server = createServer(this.app);
 
